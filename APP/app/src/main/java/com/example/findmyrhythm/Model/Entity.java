de4ca049bc75f3ea3093f747bfe61c9805fd23ff
Daniel <d.pena@udc.es>
2020-02-21 18:18:51 +0100

    Clase entidad

---
+++ APP/app/src/main/java/com/example/findmyrhythm/Model/Entity.java
@@ -0,0 +1,33 @@
+package com.example.findmyrhythm.Model;
+
+public class Entity {
+
+    // Primary key of every entities
+    private  String id;
+
+    public Entity(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean hasId() {
+        return id != null && !id.equals("");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o)
+            return true;
+
+        if (!(o instanceof Entity))
+            return false;
+
+        Entity entity = (Entity) o;
+        return id.equals(entity.id);
+    }
+}
