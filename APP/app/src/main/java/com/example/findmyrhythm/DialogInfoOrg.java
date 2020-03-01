5ddb7a7fb73a230889bbd5bbc89c60f3067483d2
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2020-03-01 12:37:40 +0100

    Pantalla Perfil Organizador

---
+++ APP/app/src/main/java/com/example/findmyrhythm/DialogInfoOrg.java
@@ -0,0 +1,32 @@
+package com.example.findmyrhythm;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+public class DialogInfoOrg extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.activity_dialog_info_org, null));
+        builder.setTitle("Playa Club")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+}
--- APP/app/src/main/java/com/example/findmyrhythm/OrgProfile.java
+++ APP/app/src/main/java/com/example/findmyrhythm/OrgProfile.java
@@ -1,8 +1,16 @@
 package com.example.findmyrhythm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.SignInButton;
 
 public class OrgProfile extends AppCompatActivity {
 
@@ -10,5 +18,18 @@ public class OrgProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_org_profile);
+
+        ImageView infoButton = findViewById(R.id.info);
+        infoButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                DialogInfoOrg dialogo = new DialogInfoOrg();
+                dialogo.show(fragmentManager, "tagAlerta");
+            }
+        });
+
+
     }
 }
