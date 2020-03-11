004358df2341d19a27c2fc77a4dd6a2c2084d024
José Morano <j.morano.sanchez@gmail.com>
2020-03-11 19:11:46 +0100

    Añadidas clases base para la integración del menú con el resto de actividades.

---
+++ APP/app/src/main/java/com/example/findmyrhythm/View/EMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.findmyrhythm.View;
+import android.os.Bundle;
+
+import com.example.findmyrhythm.R;
+
+public class EMenuActivity extends MenuDrawerActivity {
+
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.home_menu_fragment);
+    }
+
+}
---
+++ APP/app/src/main/java/com/example/findmyrhythm/View/MenuDrawerActivity.java
@@ -0,0 +1,145 @@
+package com.example.findmyrhythm.View;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.findmyrhythm.R;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+
+public class MenuDrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        DrawerLayout.DrawerListener {
+
+    protected DrawerLayout fullLayout;
+    protected LinearLayout actContent;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    public void setContentView(final int layoutResID) {
+        // Your base layout here
+        fullLayout= (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_menu, null);
+        actContent= (LinearLayout) fullLayout.findViewById(R.id.home_content);
+
+        // Setting the content of layout your provided to the act_content frame
+        getLayoutInflater().inflate(layoutResID, actContent, true);
+        super.setContentView(fullLayout);
+
+        // here you can get your drawer buttons and define how they
+        // should behave and what must they do, so you won't be
+        // needing to repeat it in every activity class
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar_layout);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        MenuItem menuItem = navigationView.getMenu().getItem(0);
+        onNavigationItemSelected(menuItem);
+        menuItem.setChecked(true);
+
+        drawerLayout.addDrawerListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        header.findViewById(R.id.header_title).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MenuDrawerActivity.this, getString(R.string.title_click),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+        int title;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:
+                title = R.string.menu_profile;
+                break;
+            case R.id.nav_recommended:
+                title = R.string.menu_recommended;
+                break;
+            case R.id.nav_search:
+                title = R.string.menu_search;
+                break;
+            case R.id.nav_notifications:
+                title = R.string.menu_notifications;
+                break;
+            case R.id.nav_settings:
+                title = R.string.menu_settings;
+                break;
+            default:
+                throw new IllegalArgumentException("menu option not implemented!!");
+        }
+
+        Fragment fragment = HomeMenuContentFragment.newInstance(getString(title));
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.home_content, fragment).commit();
+
+        //setTitle(getString(title));
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onDrawerSlide(@NonNull View view, float v) {
+        //cambio en la posición del drawer
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View view) {
+        //el drawer se ha abierto completamente
+        Toast.makeText(this, getString(R.string.navigation_drawer_open),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View view) {
+        //el drawer se ha cerrado completamente
+    }
+
+    @Override
+    public void onDrawerStateChanged(int i) {
+        //cambio de estado, puede ser STATE_IDLE, STATE_DRAGGING or STATE_SETTLING
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            drawerLayout.openDrawer(GravityCompat.START);
+
+        return true;
+    }
+}
