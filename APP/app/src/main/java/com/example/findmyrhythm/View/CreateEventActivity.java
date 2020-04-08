70caf19ac9e97c778dc5d994d3b5d7b9cf63b934
estelarules <35808975+estelarules@users.noreply.github.com>
2020-04-08 15:55:39 +0200

    Ampliacion_CrearEvento

--- APP/app/src/main/java/com/example/findmyrhythm/View/CreateEventActivity.java
+++ APP/app/src/main/java/com/example/findmyrhythm/View/CreateEventActivity.java
@@ -1,8 +1,10 @@
 package com.example.findmyrhythm.View;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,12 +13,15 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
+import android.provider.MediaStore;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.net.Uri;
 import com.example.findmyrhythm.R;
-
+import android.widget.ImageView;
 import java.util.Calendar;
 
 public class CreateEventActivity extends AppCompatActivity {
@@ -24,7 +29,10 @@ public class CreateEventActivity extends AppCompatActivity {
     private static final String TAG = "Crear Evento";
     private int mYear, mMonth, mDay, mHour, mMinute;
     private EditText fecha,hora;
-
+    Uri imageUri;
+    private static final int PICK_IMAGE = 100;
+    static final Integer READ_EXST = 0x4;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,7 +75,13 @@ public class CreateEventActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        Button buttonPhoto = (Button)findViewById(R.id.buttonLoadPicture);
+        buttonPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
     }
 
@@ -116,6 +130,40 @@ public class CreateEventActivity extends AppCompatActivity {
         timePickerDialog.show();
     }
 
+    private void askPermissions(String permission,Integer requestCode){
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+
+                //This is called if user has denied the permission before
+                //In this case I am just asking the permission again
+                ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+
+            } else {
+
+                ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+            }
+        } else {
+            Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void openGallery() {
+        askPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,READ_EXST);
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
     @Override
     public boolean onSupportNavigateUp() {
         finish();
