2bd84829f145ec67e266180e578e05c02611d60e
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2020-04-08 12:47:07 +0200

    Cambios recuperar evento apuntado

--- APP/app/src/main/java/com/example/findmyrhythm/View/EventInfoActivity.java
+++ APP/app/src/main/java/com/example/findmyrhythm/View/EventInfoActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 
+import com.example.findmyrhythm.Model.EventService;
+import com.example.findmyrhythm.Model.Exceptions.InstanceNotFoundException;
 import com.example.findmyrhythm.Model.Spectator;
 import com.example.findmyrhythm.Model.SpectatorService;
+import com.example.findmyrhythm.Model.User;
+import com.example.findmyrhythm.Model.UserService;
 import com.example.findmyrhythm.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,9 +24,12 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EventInfoActivity extends AppCompatActivity implements OnMapReadyCallback {
     Boolean id_Joined = false;
+    SpectatorService spectatorService = new SpectatorService();
+    UserService userService = new UserService();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,9 +37,7 @@ public class EventInfoActivity extends AppCompatActivity implements OnMapReadyCa
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.layout_actionbar_empty);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
 
         setContentView(R.layout.activity_event_info);
 
@@ -41,14 +47,6 @@ public class EventInfoActivity extends AppCompatActivity implements OnMapReadyCa
 
         final Chronometer chronometer = (Chronometer) findViewById(R.id.eventClock);
         final Button joinButton = (Button) findViewById(R.id.joinBtn);
-
-        final SpectatorService spectatorService = new SpectatorService();
-
-        final Spectator spectator = spectatorService.findSpectatorByIds("FJkjdJFIOEHb7895", "PLCfp0OsCMN3MG14kcuMZoPHnwA3");
-
-        if (spectator != null){
-            id_Joined=true;
-        }
 
            if (id_Joined) {
                 joinButton.setText("Apuntado");
@@ -62,16 +60,16 @@ public class EventInfoActivity extends AppCompatActivity implements OnMapReadyCa
             @Override
             public void onClick(View v) {
                 if(id_Joined){
-                    spectatorService.deleteSpectator(spectator);
+                    //spectatorService.deleteSpectator(spectator);
                     joinButton.setText("Apuntarse");
-                    joinButton.setBackgroundColor(Color.BLUE);
+                    joinButton.setBackgroundColor(0xFFB3A1CE);
                     chronometer.stop();
                     id_Joined= false;
 
                 }else{
                     spectatorService.createSpectator("FJkjdJFIOEHb7895", "PLCfp0OsCMN3MG14kcuMZoPHnwA3");
                     joinButton.setText("Apuntado");
-                    joinButton.setBackgroundColor(Color.GREEN);
+                    joinButton.setBackgroundColor(0xFF673AB7);
                     chronometer.start();
                     id_Joined= true;
 
@@ -81,6 +79,11 @@ public class EventInfoActivity extends AppCompatActivity implements OnMapReadyCa
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new getJoined().execute();
+    }
     @Override
     public void onMapReady(GoogleMap map) {
         LatLng latLong = new LatLng(43.3713500, -8.3960000);
@@ -100,4 +103,34 @@ public class EventInfoActivity extends AppCompatActivity implements OnMapReadyCa
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+    private class getJoined extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            System.out.println("aqui1");
+            try {
+                User user = userService.getUser(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                Spectator spectator = spectatorService.findSpectatorByIds("FJkjdJFIOEHb7895", user.getId());
+                if (spectator != null){
+                    id_Joined=true;
+                }
+                System.out.println(spectator.getIdEvent() + "\n");
+                System.out.println(spectator.getIdUser() + "\n");
+            } catch (InstanceNotFoundException e) {
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
 }
+
