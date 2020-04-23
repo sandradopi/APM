3e94fda8b4897b9912f7d7709bfa15b434fc2508
Morano <j.morano.sanchez@gmail.com>
2020-04-23 13:12:04 +0200

    Sobrescritura de métodos permisos para poder ser utilizado con cualquier actividad:

--- APP/app/src/main/java/com/example/findmyrhythm/Model/Utils/PermissionUtils.java
+++ APP/app/src/main/java/com/example/findmyrhythm/Model/Utils/PermissionUtils.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
+
 import android.widget.Toast;
 import com.example.findmyrhythm.R;
 import com.example.findmyrhythm.View.SearchEventsActivity;
@@ -24,7 +26,24 @@ public abstract class PermissionUtils {
      * Requests the fine location permission. If a rationale with an additional explanation should
      * be shown to the user, displays a dialog that triggers the request.
      */
-    public static void requestPermission(SelectAddressOnMapActivity activity, int requestId,
+    public static void requestPermission(FragmentActivity activity, int requestId,
+                                         String permission, boolean finishActivity) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            // Display a dialog with rationale.
+            PermissionUtils.RationaleDialog.newInstance(requestId, finishActivity)
+                    .show(activity.getSupportFragmentManager(), "dialog");
+        } else {
+            // Location permission has not been granted yet, request it.
+            ActivityCompat.requestPermissions(activity, new String[]{permission}, requestId);
+
+        }
+    }
+
+    /**
+     * Requests the fine location permission. If a rationale with an additional explanation should
+     * be shown to the user, displays a dialog that triggers the request.
+     */
+    public static void requestPermission(AppCompatActivity activity, int requestId,
                                          String permission, boolean finishActivity) {
         if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
             // Display a dialog with rationale.
