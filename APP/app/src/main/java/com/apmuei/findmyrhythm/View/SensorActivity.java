a0677177a2a5381ecc6c9de88910ef0d0b1f873c
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2020-05-10 13:17:02 +0200

    Esqueleto sensor de luz

---
+++ APP/app/src/main/java/com/apmuei/findmyrhythm/View/SensorActivity.java
@@ -0,0 +1,52 @@
+package com.apmuei.findmyrhythm.View;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import com.apmuei.findmyrhythm.R;
+
+public class SensorActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor light;
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float amountOfLight = sensorEvent.values[0];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
